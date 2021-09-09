1d9c2421fddc7443dc1935b9e2b07d6fa63ba2ad
abhishekshah <56739982+abhishekshah0036@users.noreply.github.com>
2021-09-09 12:28:30 +0530

    started working on chat

--- tinder-backend/routes/chat.js
+++ tinder-backend/routes/chat.js
@@ -24,55 +24,5 @@ chatRouter.route('/')
     },err => next(err))
     .catch((err)=> next(err))
 });
-chatRouter.route('/:matchId')
-.get(isUser,(req,res,next)=>{
-    Chat.find({chatroomId:req.userId+''+req.params.matchId})
-    .then((chat) => {
-        res.statusCode = 200;
-        res.setHeader('Content-Type','application/json');
-        res.json(chat);
-    },err => next(err))
-    .catch((err)=> next(err))
-})
-.post(isUser,(req,res,next)=>{
-    Chat.findOne({chatroomId:req.userId+' '+req.params.matchId})
-    .then((chat) =>{
-        var classId = req.userId+' '+req.params.matchId;
-        var message = req.body.message;
-        var sender  = req.userId;
-        if(chat === null){
-            Chat.create({
-                chatroomId:classId,
-                messages:{
-                    message:message,
-                    sender:sender
-                }})
-            .then((chat) =>{
-                res.statusCode = 200;
-                res.setHeader('Content-Type','application/json');
-                res.json(chat);
-            }, err => next(err) )
-        }
-        else{
-            chat.messages.push({
-                message:message,
-                sender:sender
-            });
-            chat.save();
-            res.statusCode = 200;
-            res.setHeader('Content-Type','application/json');
-            res.json(chat);
-        }
-    },err => next(err))
-    .catch(err=> next(err))
-})
-.delete(isUser,(req,res,next)=>{
-    Chat.remove({})
-        .then(resp =>{
-            res.statusCode = 200;
-            res.setHeader('Content-Type','application/json');
-            res.json(resp);
-        },err => next(err))
-        .catch(err => next(err));
-})
+
 module.exports = chatRouter;
