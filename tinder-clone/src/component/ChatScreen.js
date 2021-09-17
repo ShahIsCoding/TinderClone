86821db9f4415dd1ff0dd901f138c8191663bb58
abhishekshah <56739982+abhishekshah0036@users.noreply.github.com>
2021-09-17 23:36:02 +0530

    chat

--- tinder-clone/src/component/ChatScreen.js
+++ tinder-clone/src/component/ChatScreen.js
@@ -1,32 +1,42 @@
 import React,{useState,useEffect} from 'react'
 import { useParams } from 'react-router';
-
+import ScrollToBottom from 'react-scroll-to-bottom'; 
 import { axiosInstance } from '../axios';
 
 import { Avatar } from '@material-ui/core';
 
 function ChatScreen(props) {
     
-    const [id, setid]  = useState('');
+    const [macthedUser, setMatch]  = useState({
+        name:null,
+        imgUrl:null
+    });
     const {chatroomId} = useParams();
     const [input, setInput] = useState('');
     const [messages, setMessage] = useState([]);
 
    useEffect(() => {
-    if(id === '')
-    {    axiosInstance.get('/users/user')
-        .then(usr => setid(usr.data._id));
-    }
+        async function fetchChatroomUser(chatroomId){
+            await axiosInstance.get(`/chats/users/${chatroomId}`)
+            .then(users => {
+                console.log(users);
+                setMatch(users.data.macthedUser)
+
+            })
+            .catch(err => console.log(err))
+        }
         async function fetchMessages(chatroomId){
             await axiosInstance.get(`/chats/${chatroomId}`)
             .then(messages => setMessage(messages.data))
             .catch(err => console.log(err));
         }
+        if(macthedUser.name == null) fetchChatroomUser(chatroomId);
         fetchMessages(chatroomId);
     }, [messages]);
 
     const handleSend = e =>{
         e.preventDefault();
+        console.log(macthedUser);
         async function postMessage(){
             var req =  await axiosInstance.post(`/chats/${chatroomId}`,{
                 message:input
@@ -37,15 +47,26 @@ function ChatScreen(props) {
     }
     return (
         <div className='chatScreen'>
-            <p className='chatScreen__timestamp'>{id} have matched with Sai on  {chatroomId}  01/08/2021</p>
-            <div className='chatScreen__message' >
+            <p className='chatScreen__timestamp'>You have matched with {macthedUser.name} </p>
+            <ScrollToBottom className='chatScreen__message' >
                 {messages.map((message)=>
                     (!message.sender)?
-                       ( <p className='chatScreen__text'>{message.message}</p>)
-                    :
-                        (<p className='chatScreen__textUser'>{message.message}</p>)
+                       (
+                        <div className="chatScreen__text chatScreen__text--match">
+                            <Avatar
+                            className="chatScreen__image"
+                            alt={macthedUser.name}
+                            src={macthedUser.imgUrl}
+                            />
+                            <p >{message.message}</p>
+                        </div>
+                       ):(
+                        <div className="chatScreen__text chatScreen__text--User">
+                            <p>{message.message}</p>
+                        </div>
+                        )
                 )}
-            </div>
+            </ScrollToBottom>
             
             <form className='chatScreen__input' onSubmit={(e) => handleSend(e)}>
                 <input className='chatScreen__inputField'
